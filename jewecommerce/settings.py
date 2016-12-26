edb504c00d358493f5e8b2418211242994cdaf0b
tyrieljs <tyrieljs@gmail.com>
2016-12-26 17:06:34 -0200

    changes in settings

--- jewecommerce/settings.py
+++ jewecommerce/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os dj_database_url
+import os 
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
