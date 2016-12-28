ba30867dc58df27f9ccbb3089408f611630fbc4e
tyrieljs <tyrieljs@gmail.com>
2016-12-28 16:59:39 -0200

    revisão procfile

--- jewecommerce/wsgi.py
+++ jewecommerce/wsgi.py
@@ -1,10 +1,4 @@
-"""
-WSGI config for jewecommerce project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""WSGI config for djangoecommerce projects
 """
 
 import os
@@ -13,4 +7,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jewecommerce.settings")
 
-application = get_wsgi_application()
+application =  get_wsgi_application()
