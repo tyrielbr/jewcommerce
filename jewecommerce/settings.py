bc3e2d45d28762627086f01ff80676657c0a30db
tyrieljs <tyrieljs@gmail.com>
2016-12-28 17:13:44 -0200

    revisão settings

--- jewecommerce/settings.py
+++ jewecommerce/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'jewcommerce.urls'
+ROOT_URLCONF = 'jewecommerce.urls'
 
 TEMPLATES = [
     {
